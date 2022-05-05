a9b2aa30cbcf533fcfa42b324462d18280498fbc
Henry <lkhh@protonmail.com>
2022-05-05 17:16:49 +0800

    Update overall page

--- src/routes/data/overall.svelte
+++ src/routes/data/overall.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
-	import { DownloadIcon, UserIcon } from '@rgossiaux/svelte-heroicons/outline';
+	import {
+		ChevronDoubleRightIcon,
+		ChevronRightIcon,
+		DownloadIcon,
+		UserIcon
+	} from '@rgossiaux/svelte-heroicons/outline';
 
 	let data = [
 		{
+			id: 0,
 			region: {
 				name: 'France',
 				code: 'FR',
@@ -10,7 +16,20 @@
 					{
 						name: 'Paris',
 						code: '75',
-						cities: []
+						cities: [
+							{
+								name: 'Paris',
+								code: '75001',
+								lastInput: 100000,
+								formCount: 1000,
+								voterCount: 100000,
+								update: 345678
+							}
+						],
+						lastInput: 100000,
+						formCount: 1000,
+						voterCount: 100000,
+						update: 345678
 					}
 				]
 			},
@@ -23,17 +42,17 @@
 </script>
 
 <tbody>
-	{#each data as dataPoint}
+	{#each data as dataPoint (dataPoint.id)}
 		<tr class="divide-x-1 divide-black text-center">
 			<td class="p-4 flex">
 				<div class="flex items-center gap-2 flex-grow flex-shrink-0">
 					<div class="h-6 w-6 bg-indigo-400 rounded-full p-1 text-white">
 						<UserIcon />
 					</div>
-					<p>Hong Kong</p>
+					<p>{dataPoint.region.name}</p>
 					<button class="h-6 w-6 bg-green-400 rounded p-1"><DownloadIcon /></button>
 				</div>
-				<button class="rounded bg-gray-200 p-1"
+				<button class="rounded bg-gray-200 p-1 flex-shrink-0"
 					>{dataPoint.region.districts.length} districts</button
 				>
 			</td>
@@ -42,5 +61,50 @@
 			<td>{dataPoint.voterCount}</td>
 			<td>{dataPoint.update}</td>
 		</tr>
+		{#each dataPoint.region.districts as district}
+			<tr class="divide-x-1 divide-black text-center">
+				<td class="p-4 flex">
+					<div class="flex items-center gap-2 flex-grow flex-shrink-0">
+						<div class="h-3 w-3">
+							<ChevronRightIcon />
+						</div>
+						<div class="h-6 w-6 bg-indigo-400 rounded-full p-1 text-white">
+							<UserIcon />
+						</div>
+						<p>{district.name}</p>
+						<button class="h-6 w-6 bg-green-400 rounded p-1"><DownloadIcon /></button>
+					</div>
+					<button class="rounded bg-gray-200 p-1 flex-shrink-0"
+						>{district.cities.length}
+						<span class="hidden sm:inline">cities</span></button
+					>
+				</td>
+				<td>{district.lastInput}</td>
+				<td>{district.formCount}</td>
+				<td>{district.voterCount}</td>
+				<td>{district.update}</td>
+			</tr>
+			{#each district.cities as city}
+				<tr class="divide-x-1 divide-black text-center">
+					<td class="p-4 flex">
+						<div class="flex items-center gap-2 flex-grow flex-shrink-0">
+							<div class="w-1" />
+							<div class="h-3 w-3">
+								<ChevronDoubleRightIcon />
+							</div>
+							<div class="h-6 w-6 bg-indigo-400 rounded-full p-1 text-white">
+								<UserIcon />
+							</div>
+							<p>{city.name}</p>
+							<button class="h-6 w-6 bg-green-400 rounded p-1"><DownloadIcon /></button>
+						</div>
+					</td>
+					<td>{city.lastInput}</td>
+					<td>{city.formCount}</td>
+					<td>{city.voterCount}</td>
+					<td>{city.update}</td>
+				</tr>
+			{/each}
+		{/each}
 	{/each}
 </tbody>
